59b3a7ef0a5816f829ffab3af3b9e8a57e036486
Wookhwang <46039975+Wookhwang@users.noreply.github.com>
2019-12-29 01:54:39 +0900

    Add files via upload

---
+++ 10828.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string str;
+	int N;
+	
+	cin >> N;
+	
+	while (N--) {
+		cin >> str;
+
+		if(strcmp(str, "push"))
+	}
+}
