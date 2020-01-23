d837a05132d3240514afcc55a3c08f6ec075bee5
Wookhwang <46039975+Wookhwang@users.noreply.github.com>
2020-01-23 14:36:32 +0900

    cat

---
+++ 10171.cpp
@@ -0,0 +1,20 @@
+/*
+BaekJoon 10171
+Problem name : 고양이
+category : 입출력
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+	return 0;
+}
+
